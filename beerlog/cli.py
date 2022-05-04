c8a00602a3dc73ef7a39b9c968c9e8d6a5afdc26
marfebr <marfebr@gmail.com>
2022-05-04 16:39:10 +0000

    cli

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,22 @@
-from .config import settings
+import typer
 
 
-def main():
-    print("Hello from", settings.NAME)
+main = typer.Typer(help="Beer Manager Application")
+
+
+@main.command("add")
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+    ):
+    "Adiciona uma nova cerveja"
+    print(name, style)
+
+
+@main.command("list")
+def list_beer(style: str):
+    "Lista cervejas de um stilo"
+    print(style)
